eddcb1e6f3474e6676560c18b246fa88f780d22f
ARYATONY <aryatonys01@gmail.com>
2026-01-10 16:33:33 +0800

    new

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -22,9 +22,11 @@ app.get('/', (req, res) => {
     res.send('Inventory API is running...');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
 
